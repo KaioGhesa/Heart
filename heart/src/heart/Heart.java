d08ec5a27a5159faf1f072a3ad3fbc427cf95e13
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-04-04 13:42:47 -0300

    Add files via upload

---
+++ heart/src/heart/Heart.java
@@ -0,0 +1,16 @@
+package heart;
+public class Heart {
+    public static void main(String[] args) {
+    System.out.println("    ***        ***");
+        System.out.println("  *****      *****");
+        System.out.println(" *******    *******");
+        System.out.println("*********  *********");
+        System.out.println("*********************");
+        System.out.println(" *******************");
+        System.out.println("   ***************");
+        System.out.println("     ***********");
+        System.out.println("       *******");
+        System.out.println("         ***");
+        System.out.println("          *");
+    }
+}
